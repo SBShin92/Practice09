d2d10687239e8c24bb1402c321413a15fdd468b7
SBShin92 <aopaoi0987@gmail.com>
2024-04-05 18:04:51 +0900

    Done

--- src/com/javaex/ex04/RectTriangle.java
+++ src/com/javaex/ex04/RectTriangle.java
@@ -1,8 +1,26 @@
 package com.javaex.ex04;
 
-public class RectTriangle {
+public class RectTriangle extends Shape {
     private double width;
     private double height;
 
-    
+    public RectTriangle(double width, double height) {
+        super(3);
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return ((width * height) / 2);
+    }
+
+    @Override
+    public double getPerimeter() {
+
+        double tmp = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
+
+
+
+        return (tmp + width + height);
+    }
 }
