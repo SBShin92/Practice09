d6f3ee42544cf8ef988ebc6ce28765bb51c3a846
Sangbeom Shin <aopaoi0987@gmail.com>
2024-04-05 19:45:33 +0900

    fix some exception control

--- src/com/javaex/ex03/GoodsApp.java
+++ src/com/javaex/ex03/GoodsApp.java
@@ -1,7 +1,6 @@
 package com.javaex.ex03;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,31 +11,30 @@ public class GoodsApp {
         String inputLine;
         String[] splitLine;
 
-        System.out.println("상품을 입력해주세요(종료 q)");
+        System.out.println("상품을 입력해주세요(종료 q)[ 입력예시>> LG그램,9000,5 ]");
         while (true) {
-            inputLine = sc.nextLine();
+            inputLine = sc.nextLine().trim();
             if (inputLine.equals("q"))
                 break;
-            // isValue????    진짜 넣긴 해야하는데 ...
             splitLine = inputLine.split(",");
             Goods tmp = new Goods();
             tmp.setName(splitLine[0]);
             try {
                 tmp.setPrice(Integer.parseInt(splitLine[1]));
                 tmp.setCount(Integer.parseInt(splitLine[2]));
+                goodsList.add(tmp);
             } catch (NumberFormatException e) {
                 System.err.println("잘못된 입력");
             }
-            goodsList.add(tmp);
         }
         System.out.println("[입력완료]");
-        Iterator<Goods> iter = goodsList.iterator();
+
         int sum = 0;
-        while (iter.hasNext()) {
-            Goods tmp = iter.next();
-            tmp.informIncoming();
-            sum += tmp.getCount();
+        for (Goods node: goodsList) {
+            node.informIncoming();
+            sum += node.getCount();
         }
+
         System.out.printf("모든 상품의 갯수는 %d개입니다.%n", sum);
 
         sc.close();
